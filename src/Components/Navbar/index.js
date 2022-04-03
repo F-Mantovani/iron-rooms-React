994a20d43fc5cf505bf03ab5f8f2f6173c471425
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-03 02:05:20 +0200

    chore: navbar skeleton

--- src/Components/Navbar/index.js
+++ src/Components/Navbar/index.js
@@ -1,9 +1,19 @@
-import React from 'react'
+import React from "react";
+import { NavLink } from "react-router-dom";
 
 const index = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav>
+      <h2>Iron Rooms</h2>
+      <div>
+        <NavLink to='/'> All Rooms</NavLink>
+        {' '}
+        <NavLink to="/"> My Rooms</NavLink>
+        {' '}
+        <NavLink to="/"> My Profile</NavLink>
+      </div>
+    </nav>
+  );
+};
 
-export default index
+export default index;
