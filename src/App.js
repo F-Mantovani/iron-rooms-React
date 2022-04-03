990bb4362bb0928e9ff17a4d8174cc123fb9d4bc
Felipe Leite Mantovani <felipe.mantovani@outlook.com>
2022-04-03 02:15:27 +0200

    chore: add routes structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import "./App.css";
+import { Routes, Route } from "react-router-dom";
 import Navbar from "./Components/Navbar";
+import PrivateOutlet from "./Components/PrivateOutlet";
+import AllRooms from "./Pages/AllRooms";
+import MyRooms from "./Pages/MyRooms";
+import Profile from "./Pages/Profile";
+import Login from "./Pages/Login";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Login />}></Route>
+        <Route path="/private" element={<PrivateOutlet />}>
+          <Route path="/private/allrooms" element={<AllRooms />}></Route>
+          <Route path="/private/myrooms" element={<MyRooms />}></Route>
+          <Route path="/private/profile" element={<Profile />}></Route>
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/Components/PrivateOutlet.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import { Navigate, Outlet } from 'react-router-dom'
+
+const PrivateOutlet = () => {
+  const auth = localStorage.getItem('token')
+  return auth ? <Outlet /> : <Navigate to ='/' />
+}
+
+export default PrivateOutlet
